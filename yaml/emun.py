276dbd1a8f03852fbbc8714aaabe50335b743852
feixiao <feixiao2020@sina.com>
2023-11-16 16:56:03 +0800

    update

--- yaml/emun.py
+++ yaml/emun.py
@@ -21,14 +21,6 @@ class MyEnum2(enum.Enum):
 # 自定义输出函数
 
 
-# 自定义输出函数，去掉类名的前缀部分
-def custom_representer(dumper, data):
-    class_name = data.__class__.__name__
-    if '.' in class_name:
-        class_name = class_name.split('.')[-1]  # 获取最后一个部分作为类名
-    return dumper.represent_scalar('tag:yaml.org,2002:str', class_name)
-
-
 def enum_representer(dumper, data):
     if isinstance(data, MyEnum1):
         return dumper.represent_scalar('tag:yaml.org,2002:str', f'{data.__class__.__name__}.MyEnum1.{data.name}')
@@ -36,16 +28,13 @@ def enum_representer(dumper, data):
         return dumper.represent_scalar('tag:yaml.org,2002:str', f'MyEnum2')
 
 
-# 将自定义输出函数注册到Representer中
-yaml.add_representer(MyEnum1, custom_representer)
-yaml.add_representer(MyEnum1, custom_representer)
-
 # 创建包含两种枚举类的字典
 enum_dict = {
     'value_1': MyEnum1.FIRST,
     'value_2': MyEnum2.B,
     'value_3': MyEnum1.THIRD,
-    'value_4': None
+    'value_4': None,
+    'value_5': MyEnum1.THIRD  # 会引用之前的枚举值 &id001 => *id001
 }
 
 # 将字典中的枚举值转储为 YAML 文件
