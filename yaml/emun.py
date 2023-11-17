814c9859142a73a9d58d865b18b331161be694c6
feixiao <feixiao2020@sina.com>
2023-11-17 11:24:33 +0800

    支持yaml自定义输出

--- yaml/emun.py
+++ yaml/emun.py
@@ -29,7 +29,7 @@ def enum_representer(dumper, data):
 
 
 # 定制输出函数来避免输出 !!python/object/apply:__main__.MyEnum2
-def custom_represent_none(dumper, data):
+def custom_represent_none(dumper):
     return dumper.represent_scalar('tag:yaml.org,2002:null', '')
 
 
@@ -46,6 +46,43 @@ enum_dict = {
     'value_5': MyEnum1.THIRD  # 会引用之前的枚举值 &id001 => *id001
 }
 
+enum_dict1 = {
+    'value_1': MyEnum1.FIRST,
+    'value_2': MyEnum2.B,
+    'value_3': MyEnum1.THIRD,
+    'value_4': None,
+    'value_5': MyEnum1.THIRD  # 会引用之前的枚举值 &id001 => *id001
+}
+
+
+data = []
+data.append(enum_dict)
+data.append(enum_dict1)
+
+
+def represent_myclass(dumper, data):
+    return dumper.represent_mapping('tag:yaml.org,2002:map', {
+        'name': data.name,
+        'data': data.data
+    })
+
+
+class MyClass:
+    def __init__(self, name, data):
+        self.name = name
+        self.data = data
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return self.name
+
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, MyClass):
+            return self.name == o.name
+        return False
+
 
 class MySafeDumper(yaml.SafeDumper):
     def represent_data(self, data):
@@ -56,8 +93,15 @@ class MySafeDumper(yaml.SafeDumper):
         return super().represent_data(data)
 
 
+MySafeDumper.add_representer(MyClass, represent_myclass)
+
 # 将字典中的枚举值转储为 YAML 文件
 # with open('multiple_enums.yaml', 'w') as file:
 output_yaml = yaml.dump(
-    enum_dict, Dumper=MySafeDumper, default_flow_style=False, allow_unicode=True)
+    data, Dumper=MySafeDumper, default_flow_style=False, allow_unicode=True)
+print(output_yaml)
+
+object = MyClass("test", data)
+output_yaml = yaml.dump(object, Dumper=MySafeDumper,
+                        default_flow_style=False, allow_unicode=True)
 print(output_yaml)
